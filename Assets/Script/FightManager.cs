a9b20f9382243382cebabf3ee42bbbc4ba2075d6
Ludovic Brun <lubrun@z1r10p5.le-101.fr>
2019-02-21 23:21:32 +0100

    First commit

---
+++ Assets/Script/FightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour
+{
+    public GameObject soldier;
+    public GameObject floor;
+    public Material soldierMaterial;
+    private GameObject selected;
+    private Camera cam;
+    void Start()
+    {
+        selected = null;
+        cam = Camera.main;
+        Instantiate(floor, new Vector3(0, 0, 0), Quaternion.identity); 
+        Instantiate(soldier, new Vector3(0, 5, 0), Quaternion.identity); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Soldier")) {
+                if (selected != null)
+                    selected.GetComponent<Renderer>().material = soldierMaterial;
+                selected = hit.collider.gameObject;
+                selected.GetComponent<Renderer>().material.color = Color.green;
+            } else if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Floor") && selected != null) {
+                selected.GetComponent<Soldier>().setLocToGo(hit.point);
+                selected.GetComponent<Renderer>().material = soldierMaterial;
+                selected = null;
+            } else if (Physics.Raycast(ray, out hit) && hit.collider == null && selected != null) {
+                selected.GetComponent<Renderer>().material = soldierMaterial;
+                selected = null;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Soldier.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    public float speed = 10f;
+    public float hoverForce = 20f;
+    public float hoverHeight = 3.5f;
+    private Rigidbody soldierRigidbody;
+    public Vector3 locToGo;
+    void Awake()
+    {
+        soldierRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (transform.position.x == locToGo.x && transform.position.z == locToGo.z)
+            setLocToGo(new Vector3(-8000, -8000, -8000));
+        else if (locToGo.x != -8000) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, locToGo, step);
+        }
+    }
+    void FixedUpdate()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, hoverHeight))
+        {
+            float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+            Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+            soldierRigidbody.AddForce(appliedHoverForce, ForceMode.Acceleration);
+        }
+    }
+
+    public void setLocToGo(Vector3 new_loc)
+    {
+        locToGo = new_loc;
+    }
+}
